e44489f20f166c678ffc9243d82b8fd8983ef96e
Expirty <expirty.bu@gmail.com>
2017-11-17 02:26:47 +0300

    first

---
+++ WebApplication5/Controllers/MyController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class MyController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult registerForm(string name, string email, string username, string password, string cpassword)
+        {
+            return View(new Members(name, username, password, email, cpassword));
+        }
+        public ActionResult loginForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult loginForm(string username, string password)
+        {
+            bool flag = false;
+            foreach (Members m in Members.mList)
+            {
+                if(username == m.userName && password == m.passWord)
+                {
+                    //send the other page
+                    m.state = "User has been found.";
+                    View(m);
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag)
+            {
+                View();
+            }
+            return View();
+        }
+    }
+}
---
+++ WebApplication5/Models/members.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class Members
+    {
+        public static List<Members> mList = new List<Members>();
+        string name;
+        string username;
+        string password = null;
+        string cPassword;
+        bool c;
+        public string state;
+        Trails trail; 
+        public Members(string name, string username, string password, string email, string cPassword)
+        {
+            this.name = name;
+            this.username = username;
+            this.password = password;
+            c = false;
+            this.cPassword = cPassword;
+            state = " ";
+            
+        }
+        public bool ControlAccount
+        {
+            get
+            {
+                return c;
+            }
+        }
+        public string userName
+        {
+            get
+            {
+                return username;
+            }
+        }
+        public string passWord
+        {
+            get
+            {
+                return password;
+            }
+        }
+        public string Announ(string error, string code)
+        {
+            switch(error)
+            {
+                case "emptyfield":
+                {
+                        switch(code)
+                        {
+                            case "email": return "You have to input a correct e-mail adress.";
+                            case "username": return "You have to input a username.";
+                            case "password": return "You have to input a password.";
+                        }
+                        return "An error occured.";
+                }
+                case "PasswordNotConfirmed": return "The password you entered is not confirmed.";
+            }
+            mList.Add(this);
+            c = true;
+            return "Registration is completed successfully.";
+
+        }
+        public string Check()
+        {
+            if (username == "") return Announ("emptyfield", "username");
+            if (name == "") return Announ("emptyfield", "email");
+            if (password == "") return Announ("emptyfield", "password");
+            //if (password.CompareTo(cPassword) != 0) return Announ("PasswordNotConfirmed", "none");
+            Trails tObject = new Trails();
+            return Announ("success", "sc");
+
+        }
+
+    }
+        
+}
